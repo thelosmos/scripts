2bf7a39fac8573d4fb20373c0af3aee33796279d
Carlos Ramos <4458629+thelosmos@users.noreply.github.com>
2023-01-05 15:43:09 -0500

    web-to-pf.sh first commit

---
+++ WebToPDF/web-to-pdf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Pipe URL list to this script and it will output a pdf(s) of the pages in the current directory. This scrip assumes the URL(s) have an htm extension. Please modify as needed.
+# Depends on Chromium being installed
+
+while read line; 
+    do filename=$(echo $line | grep -iPo '[^/\\&\?]+\.\w{3,4}(?=([\?&].*$|$))' | sed 's/htm/pdf/'); 
+        chromium --headless  --print-to-pdf=$filename $line;
+done  
+
