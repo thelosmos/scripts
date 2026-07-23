af10ee3c57121f6ef1105e3588282eb4e6d0043b
Carlos Ramos <34458629+thelosmos@users.noreply.github.com>
2026-07-23 17:41:08 -0400

    ts

--- MacOS/DisplayChanger.sh
+++ MacOS/DisplayChanger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DP="/opt/homebrew/bin/displayplacer list"
+DP="/opt/homebrew/bin/displayplacer"
 SCREEN_1="6AA432C0-05F6-4C60-8E10-1C16192D6944" # Dell 34" Widescreen Bottom
 SCREEN_2="59F17B16-BAC7-4310-865F-C0E5B9FE6F9C" # Dell 27" Widescreen Portrait Right
 SCREEN_3="A7D583A8-6DBE-42F9-9C2E-252B7975FC46" # LG 34" Widescreen Top
